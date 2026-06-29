ea8f3735ff550fa803a48afb5c9f92d0f3ccf51a
Sarah Garcia de Carvalho <sarahgarcia.carvalho@gmail.com>
2026-06-29 11:04:36 -0300

    Refactor two sums logic to use index range

--- 03_twosums.py
+++ 03_twosums.py
@@ -3,11 +3,11 @@ numeros = [2,7,11,15]
 indices = []
 primeiro_num = None
 
-for i in numeros: 
-    primeiro_num = i 
-    faltante = 9 - i 
-    for x in numeros: 
-        if x == faltante and x not in indices: 
+for i in range(len(numeros)): 
+    primeiro_num = numeros[i] 
+    faltante = 9 - numeros[i] 
+    for x in range(len(numeros)): 
+        if numeros[x] == faltante and x not in indices: 
             indices.append(i)
             indices.append(x)
 
